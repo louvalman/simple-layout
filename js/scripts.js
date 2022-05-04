7d21e7a854b36195d5393756c94536aa40d87fa6
louvalman <94385943+louvalman@users.noreply.github.com>
2022-05-04 02:00:11 +0200

    sidenav

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 let sectionMain = document.getElementById("section-main");
 let navbar = document.getElementById("navbar");
-
 let navPos = navbar.getBoundingClientRect().top;
 
 window.addEventListener("scroll", e => {
@@ -14,3 +13,34 @@ window.addEventListener("scroll", e => {
     sectionMain.classList.remove('navbarOffsetMargin');
   }
 });
+
+window.addEventListener("scroll", e => {
+  let viewportHeight = window.innerHeight;
+  let scrollPos = window.scrollY;
+  if (scrollPos > navPos) {
+    sidenavid.classList.add('sticky');
+  } else {
+    sidenavid.classList.remove('sticky');
+  }
+});
+
+function toggleNav() {
+  var element = document.getElementById("sidenavid");
+  if (element.style.width == "250px"){
+    element.style.width = "0px";
+  } else {
+    element.style.width = "250px";
+  }
+  var element = document.getElementById("main");
+  if (element.style.marginLeft == "250px"){
+    element.style.marginLeft = "0px";
+  } else {
+    element.style.marginLeft = "250px";
+  }
+}
+
+
+function toggleIcon(x) {
+  x.classList.toggle("fa-xmark");
+  x.classList.toggle("fa-bars");
+}
