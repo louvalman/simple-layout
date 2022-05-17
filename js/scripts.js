f61842bac39f389e8a185c2c8dc81e9985ac5c50
louvalman <94385943+louvalman@users.noreply.github.com>
2022-05-18 00:37:50 +0200

    slick slider and ui

--- js/scripts.js
+++ js/scripts.js
@@ -92,5 +92,40 @@ window.onclick = function(event) {
 
 $(document).ready(function(){
   $('.slick-content').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: $('.prev-arrow'),
+  nextArrow: $('.next-arrow'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   });
 });
