476216cde8c22e3df09951a7d338211cff749326
louvalman <louisdyrhauge@hotmail.com>
2022-05-18 21:42:45 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -103,7 +103,7 @@ $(document).ready(function(){
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
         dots: false
