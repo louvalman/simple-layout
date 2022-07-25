b7037019856e2db5ec0eaf87316f1982f16a1618
louvalman <94385943+louvalman@users.noreply.github.com>
2022-07-26 00:16:37 +0200

    faq

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -149,3 +149,22 @@ $(document).ready(function(){
   ]
   });
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panelAccordion = this.nextElementSibling;
+    if (panelAccordion.style.display === "block") {
+      panelAccordion.style.display = "none";
+    } else {
+      panelAccordion.style.display = "block";
+    }
+  });
+}
