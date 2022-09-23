750e58f389692a1a29ea204c0bc9466c17034faa
louvalman <94385943+louvalman@users.noreply.github.com>
2022-09-23 13:06:41 +0700

    various backend and frontend

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 
 //middlewares
 app.use(express.static(staticPath));
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('listening on port 3000.......');
@@ -28,6 +29,44 @@ app.get('/signup', (req, res) => {
     res.sendFile(path.join(staticPath, "signup.html"));
 })
 
+app.post('/signup', (req, res) => {
+    let {
+        name,
+        email,
+        password,
+        number,
+        tac,
+        notification
+    } = req.body;
+
+    // form validations
+    if (name.length < 3) {
+        return res.json({
+            'alert': 'name must be 3 letters long'
+        });
+    } else if (!email.length) {
+        return res.json({
+            'alert': 'enter your email'
+        });
+    } else if (password.length < 8) {
+        return res.json({
+            'alert': 'password should be 8 letters long'
+        });
+    } else if (!number.length) {
+        return res.json({
+            'alert': 'enter your phone number'
+        });
+    } else if (!Number(number) || number.length < 10) {
+        return res.json({
+            'alert': 'invalid number, please enter valid one'
+        });
+    } else if (!tac) {
+        return res.json({
+            'alert': 'you must agree to our terms and conditions'
+        });
+    }
+})
+
 // 404 route
 app.get('/404', (req, res) => {
     res.sendFile(path.join(staticPath, "404.html"));
@@ -36,3 +75,10 @@ app.get('/404', (req, res) => {
 app.use((req, res) => {
     res.redirect('/404');
 })
+
+const processData = (data) => {
+    loader.style.display = null;
+    if (data.alert) {
+        showAlert(data.alert);
+    }
+}
