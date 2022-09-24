4ac19e255d2242d787f451e2ea8918de467fc1c6
louvalman <94385943+louvalman@users.noreply.github.com>
2022-09-24 13:34:14 +0700

    loader visible on submit

--- public/js/form.js
+++ public/js/form.js
@@ -10,31 +10,31 @@ const tac = document.querySelector('#terms-and-cond');
 const notification = document.querySelector('#notification');
 
 submitBtn.addEventListener('click', () => {
-    //    if (name.value.length < 3) {
-    //        showAlert('name must be 3 letters long');
-    //    } else if (!email.value.length) {
-    //        showAlert('enter your email');
-    //    } else if (password.value.length < 8) {
-    //        showAlert('password should be 8 letters long');
-    //    } else if (!number.value.length) {
-    //        showAlert('enter your phone number');
-    //    } else if (!Number(number.value) || number.value.length < 10) {
-    //        showAlert('invalid number, please enter valid one');
-    //    } else if (!tac.checked) {
-    //        showAlert('you must agree to our terms and conditions');
-    // } else {
-    // submit form
-    loader.style.display = 'block';
-    sendData('/signup', {
-        name: name.value,
-        email: email.value,
-        password: password.value,
-        number: number.value,
-        tac: tac.checked,
-        notification: notification.checked,
-        seller: false
-    })
-    //    }
+    if (name.value.length < 3) {
+        showAlert('name must be 3 letters long');
+    } else if (!email.value.length) {
+        showAlert('enter your email');
+    } else if (password.value.length < 8) {
+        showAlert('password should be 8 letters long');
+    } else if (!number.value.length) {
+        showAlert('enter your phone number');
+    } else if (!Number(number.value) || number.value.length < 10) {
+        showAlert('invalid number, please enter valid one');
+    } else if (!tac.checked) {
+        showAlert('you must agree to our terms and conditions');
+    } else {
+        // submit form
+        loader.style.display = 'block';
+        sendData('/signup', {
+            name: name.value,
+            email: email.value,
+            password: password.value,
+            number: number.value,
+            tac: tac.checked,
+            notification: notification.checked,
+            seller: false
+        })
+    }
 })
 
 // send data function
