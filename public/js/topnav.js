9866c5bca9ac4779460a70cec3ef07b1eef804b4
louvalman <94385943+louvalman@users.noreply.github.com>
2022-10-02 23:49:40 +0700

    circle style no more FA

--- public/js/topnav.js
+++ public/js/topnav.js
@@ -35,7 +35,7 @@ const createtopNav = () => {
     <button class="wallet-container btn draw-border" id="connect-open">
         <span class="wallet-container-inline" data-tooltip="Network status: Good"
             data-tooltip-location="bottom">
-            <i class="fas fa-circle"></i>
+            <?xml version="1.0" encoding="UTF-8"?><svg width="11px" height="11px" stroke-width="4" stroke="#E33232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24" fill="red" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"></path></svg>
         </span>
         <text>Connect</text>
     </button>
