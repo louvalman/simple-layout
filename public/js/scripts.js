7ad3403c1d2427b5c8bfd5f880cda0ae777f4967
louvalman <94385943+louvalman@users.noreply.github.com>
2022-10-20 01:19:05 +0700

    nofill, gradient, new fonts

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -27,7 +27,7 @@ gsap.to(".bgfixed .bg", {
     end: "bottom bottom",
     scrub: 1
   },
-  filter: "hue-rotate(20deg)",
+  filter: "hue-rotate(50deg)",
   rotation: 180
 })
 
@@ -158,6 +158,47 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 }
+
+// Slick carousel 
+
+$(document).ready(function () {
+  $('.slick-content').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 1200,
+    variableWidth: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: $('.prev-arrow'),
+    nextArrow: $('.next-arrow'),
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
+  });
+});
 
 // FAQ accordion functionality
 
