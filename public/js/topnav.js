0be58c91f6f1513493e098a1d657c363b9633fba
louvalman <94385943+louvalman@users.noreply.github.com>
2022-11-01 23:14:06 +0700

    accesibility and perfomance

--- public/js/topnav.js
+++ public/js/topnav.js
@@ -10,7 +10,8 @@ const createtopNav = () => {
         <h1>Simple Layout</h1>
     </a>
 </div>
-<ul class="nav-list" id="navlist">
+<div class="nav-list" id="navlist">
+<ul>
     <div class="nav-listcontainer remove-mobile">
         <li class="nav-listitems remove-mobile"><a href="#">Overview</a></li>
         <li class="nav-listitems dropdown remove-mobile"><a href="#">Products</a>
@@ -27,14 +28,15 @@ const createtopNav = () => {
             </div>
         </li>
     </div>
-    <button class="wallet-container btn draw-border" id="connect-open">
-        <span class="wallet-container-inline" data-tooltip="Network status: Good"
-            data-tooltip-location="bottom">
-            <?xml version="1.0" encoding="UTF-8"?><svg width="11px" height="11px" stroke-width="4" stroke="#E33232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24" fill="red" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"></path></svg>
-        </span>
-        <text>Connect</text>
-    </button>
 </ul>
+<button class="wallet-container btn draw-border" id="connect-open">
+<span class="wallet-container-inline" data-tooltip="Network status: Good"
+    data-tooltip-location="bottom">
+    <?xml version="1.0" encoding="UTF-8"?><svg width="11px" height="11px" stroke-width="4" stroke="#E33232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24" fill="red" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"></path></svg>
+</span>
+<text>Connect</text>
+</button>
+</div>
     `;
 }
 
